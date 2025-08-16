97857a7f20fdd0c1ae47bde55442ebf850f83a28
Rodrigo <rodrigoplaceres19@gmail.com>
2025-08-16 16:51:24 -0300

    deleted test line index

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/my-repository/',
     plugins: [react()],
     test: {
         globals: true,
